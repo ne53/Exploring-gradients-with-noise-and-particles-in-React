b31f0a0ea0cdb4ee1b7a4c400dea5409387bbfd6
ne53 <neko252222@gmail.com>
2023-11-11 03:04:50 +0900

    Add mobile device error message and crying face emoji, update styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,16 @@ import { BrowserView, MobileView  } from "react-device-detect";
 export default function App() {
   return (
     <>
+    <div className="App">
       <MobileView>
-         <p>Sorry, this app is not available on mobile devices.</p>
+        <img src="https://raw.githubusercontent.com/twitter/twemoji/8e58ae4745075d4faa5b9190eab578aa7e4c32d5/svg/1f622.svg" alt="Crying Face" className='crying-face'/>
+        <p>Sorry, this site is not available on mobile devices.</p>
       </MobileView>
-
       <BrowserView>
-        <div className="App">
           <Noise />
           <ParticleCanvas />
-        </div>
       </BrowserView>
+      </div>
     </>
   );
 }
