7228c7b54820b1fc9340a09fbffc53d3d6c55441
neko-san <106882408+ne53@users.noreply.github.com>
2023-11-15 22:20:24 +0900

    Update particle canvas and app blur filter.

--- src/components/Gradation.tsx
+++ src/components/Gradation.tsx
@@ -15,29 +15,27 @@ type Particle = {
 };
 
 const ParticleCanvas: React.FC = () => {
-  // Reference to the canvas container
   const canvasRef = useRef<HTMLDivElement>(null);
-  // Reference to the current p5 instance
-  const p5InstanceRef = useRef<p5 | null>(null);
 
   useEffect(() => {
-    // p5 sketch function
     const sketch = (p: p5) => {
-      // Configuration settings
+      // 設定オブジェクト
       const config = {
-        count: 30,
-        minSize: 0.3,
-        maxSize: 0.5,
-        minSpeed: 0.005,
-        maxSpeed: 0.01,
-        particleColors: ["#F25EA3", "#471ED9", "#3B42D9", "#1B8EF2", "#F26241"],
-        horizontalScale: 2,
-        verticalScale: 1,
+        count: 10,                  // パーティクルの数
+        minSize: 0.3,               // パーティクルの最小サイズ
+        maxSize: 0.5,               // パーティクルの最大サイズ
+        minSpeed: 0.001,            // パーティクルの最小速度
+        maxSpeed: 0.05,            // パーティクルの最大速度
+        // bgColor: "#000000",         // 背景色
+        // particleColors: ["#FFFFFF", "#000000"],  // パーティクルの色
+        particleColors: ["#F25EA3","#471ED9", "#3B42D9", "#1B8EF2","#F26241"],  // パーティクルの色
+        horizontalScale: 2,         // 横方向のスケール
+        verticalScale: 1,           // 縦方向のスケール
       };
 
       let particles: Particle[] = [];
 
-      // Function to add a new particle
+      // パーティクルを追加する関数
       const addParticle = () => {
         const zDist = p.random() ** 3;
         const randomColor = p.random(config.particleColors);
@@ -52,12 +50,12 @@ const ParticleCanvas: React.FC = () => {
         });
       };
 
-      // Function to remove particles outside the canvas
+      // 画面外に出たパーティクルを削除する関数
       const removeOutOfCanvasParticles = () => {
         particles = particles.filter((particle) => particle.pos.x * p.width - particle.size >= 0);
       };
 
-      // Function to update particle positions
+      // パーティクルの位置を更新する関数
       const updateParticlePositions = () => {
         particles.forEach((particle) => {
           particle.pos.x -= particle.speed;
@@ -70,7 +68,7 @@ const ParticleCanvas: React.FC = () => {
         });
       };
 
-      // Function to draw particles
+      // パーティクルを描画する関数
       const drawParticles = () => {
         particles.forEach((particle) => {
           const yShift = p.map(particle.pos.y, 0, 1, -0.1, 0.1) * p.height;
@@ -87,16 +85,17 @@ const ParticleCanvas: React.FC = () => {
         });
       };
 
-      // p5 setup function
+      // 初期化
       p.setup = () => {
         const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         canvas.parent(canvasRef.current!);
-        p.frameRate(8);
+        p.frameRate(30);
       };
 
-      // p5 draw function
+      // 描画
       p.draw = () => {
         p.push();
+
         removeOutOfCanvasParticles();
         while (particles.length < config.count) {
           addParticle();
@@ -106,29 +105,17 @@ const ParticleCanvas: React.FC = () => {
         p.pop();
       };
 
-      // p5 windowResized function
+      // ウィンドウのリサイズ
       p.windowResized = () => {
         p.resizeCanvas(p.windowWidth, p.windowHeight);
       };
     };
 
-    // Remove previous p5 instance
-    if (p5InstanceRef.current) {
-      p5InstanceRef.current.remove();
-    }
-
-    // Create a new p5 instance
-    p5InstanceRef.current = new p5(sketch);
-
-    // Cleanup when the component unmounts
-    return () => {
-      if (p5InstanceRef.current) {
-        p5InstanceRef.current.remove();
-      }
-    };
+    // p5 インスタンスの生成
+    new p5(sketch);
   }, []);
 
-  // Render the particle canvas container
+  // パーティクルキャンバスの表示
   return <div ref={canvasRef} className="ParticleCanvas"></div>;
 };
 
